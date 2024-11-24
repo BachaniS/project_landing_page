c981ad5b57b53c1d8febc5c2a971eb5968c1a3cd
Somesh Bachani <someshb@Someshs-MacBook-Pro.local>
2024-11-24 03:58:22 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Header() {
+  return (
+    <div className="Head">
+      <h1>CS5610: Web Development</h1>
+      <h2>Northeastern University</h2>
+      <h2>Fall 2024</h2>
+    </div>
+  );
+}
+
+function Authors() {
+  return (
+    <div className="Authors">
+      <h3>Name: Somesh M Bachani</h3>
+      <h4>Section: 1</h4>
+      <h3>Name: Joythish Reddy</h3>
+      <h4>Section: 1</h4>
+    </div>
+  );
+}
+
+function RelevantLinks() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <p style={{ margin: "0 10px" }}>Relevant links:</p>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        {/* GeekBook */}
+        <figure style={{ margin: "0 10px" }}>
+          <a
+            className="App-link"
+            href="https://a5--cs5610kanbas.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/res/logo.png"
+              alt="GeekBook"
+              style={{ width: "30px", height: "30px" }}
+            />
+          </a>
+          <figcaption>GeekBook</figcaption>
+        </figure>
+        <span>{" || "}</span>
+        {/* GitHub */}
+        <figure style={{ margin: "0 10px" }}>
+          <a
+            className="App-link"
+            href="https://github.com/BachaniS/kanbas-react-web-app/tree/a5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              alt="GitHub"
+              style={{ width: "30px", height: "30px" }}
+            />
+          </a>
+          <figcaption>Github</figcaption>
+        </figure>
+        <span>{" || "}</span>
+        {/* GitHub Server */}
+        <figure style={{ margin: "0 10px" }}>
+          <a
+            className="App-link"
+            href="https://github.com/BachaniS/kanbas-node-server-app/tree/a1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              alt="GitHub"
+              style={{ width: "30px", height: "30px" }}
+            />
+          </a>
+          <figcaption>Github Server</figcaption>
+        </figure>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Authors />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </header>
+      <footer>
+        <RelevantLinks />
+      </footer>
     </div>
   );
 }
